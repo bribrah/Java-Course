ccacbecbb89de5a86f82889607737c85133ec08e
Bribrah <cheesemas46@gmail.com>
2018-09-24 19:47:38 -0700

    homogenized folder names

---
+++ module_7_creating_classes/Read_and_Try/bookclasses/Student.java
@@ -0,0 +1,15 @@
+public class Student
+{
+  
+  ///////////////// fields ///////////////////
+  private String name;
+  private double[] gradeArray;
+  
+  
+  /////////////methods/////////////////
+  public String toString()
+  {
+    return "Student object named : " + this.name;
+  }
+  
+}
